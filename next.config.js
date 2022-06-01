5bd322cd4e500dbc501f3bec413a49c5f94b41b2
martijn van de ree <mrtjnvanderee@gmail.com>
2022-06-01 15:36:13 +0200

    add isr

--- next.config.js
+++ next.config.js
@@ -6,5 +6,4 @@ const nextConfig = {
 module.exports = {
   distDir: 'build',
   nextConfig,
-  target: "serverless"
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -63,6 +63,7 @@ export async function getStaticProps() {
   return {
     props: {
       resources
-    }
+    },
+    revalidate: 10
   }
 }
